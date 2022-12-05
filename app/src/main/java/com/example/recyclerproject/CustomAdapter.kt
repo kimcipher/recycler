4ade94c8850f89ce15c3f42f5fd8bbdd0a301432
kimcipher <kimutaij192@gmail.com>
2022-12-05 11:43:24 +0300

    first commit

---
+++ app/src/main/java/com/example/recyclerproject/CustomAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.recyclerproject
+
+class CustomAdapter {
+}
---
+++ app/src/main/java/com/example/recyclerproject/ItemViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.recyclerproject
+
+data class ItemViewModel (val product_image:Int, val productName:String, val product_description:String,
+val product_learnMore:String){
+
+}
