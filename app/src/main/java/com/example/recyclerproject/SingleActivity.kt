8b779ed73949cda6344234f3eb7b89a8607c603d
kimcipher <kimutaij192@gmail.com>
2022-12-07 12:26:50 +0300

    mpesa api

--- app/src/main/java/com/example/recyclerproject/SingleActivity.kt
+++ app/src/main/java/com/example/recyclerproject/SingleActivity.kt
@@ -4,10 +4,17 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.JsonHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import cz.msebera.android.httpclient.entity.StringEntity
+import org.json.JSONObject
 
 class SingleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,5 +42,45 @@ class SingleActivity : AppCompatActivity() {
         Glide.with(applicationContext).load(image)
             .apply(RequestOptions().centerCrop())
             .into(imageView)
+
+        val buyBtn = findViewById<Button>(R.id.BtnSingle)
+        buyBtn.setOnClickListener {
+            val client = AsyncHttpClient(true, 80, 443)
+            val payload = JSONObject()
+            payload.put("phone", "254710638127")
+            payload.put("amount", 1)
+
+            val body = StringEntity(payload.toString())
+
+            // loopj
+
+            client.post(this, "https://ghostsec.pythonanywhere.com/payment", body, "application/json",
+
+                object: JsonHttpResponseHandler(){
+                    override fun onSuccess(
+                        statusCode: Int,
+                        headers: Array<out Header>?,
+                        response: JSONObject?
+                    ) {
+                        Toast.makeText(applicationContext, "Please Complete Payment$statusCode", Toast.LENGTH_LONG).show()
+//                        super.onSuccess(statusCode, headers, response)
+                    }
+
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Array<out Header>?,
+                        responseString: String?,
+                        throwable: Throwable?
+                    ) {
+                        Toast.makeText(applicationContext, "Please Try Again$statusCode", Toast.LENGTH_LONG).show()
+//                        super.onFailure(statusCode, headers, responseString, throwable)
+                    }
+
+
+                } // END HANDLER
+
+                )// end client
+
+        }
     }
 }
